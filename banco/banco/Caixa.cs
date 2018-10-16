74c976f192bb3a46c26e1f4520a1c8611fab4f0e
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-10-16 15:48:42 -0300

    testando associação de classes

---
+++ banco/banco/Caixa.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco
+{
+    class Caixa
+    {
+        private int codigo { get; set; }
+
+        public Caixa()
+        {
+
+        }
+
+        List<Conta> contas = new List<Conta>();
+        
+        public int criarContaCorrente(double saldo)
+        {
+            contas.Add(new ContaCorrente(contas.Count + 1, saldo));
+            return contas[contas.Count - 1].numero;
+        }
+
+        public int criarContaPoupanca(double saldo)
+        {
+            contas.Add(new ContaPoupanca(contas.Count + 1, saldo));
+            return contas[contas.Count - 1].numero;
+        }
+
+        public string setConta(int numero)
+        {
+            if (contas.Exists(x => x.numero == numero))
+            {
+                this.codigo = contas.FindIndex(x => x.numero == numero);
+                return "Conta encontrada com sucesso.";
+            }
+            else
+            {
+                return "Não existe uma conta com este numero.";
+            }
+        }
+
+        public double getSaldo()
+        {
+            return contas[this.codigo].saldo;
+        }
+
+        public void sacar(double valor)
+        {
+            contas[this.codigo].sacar(valor);
+        }
+
+        public void depositar(double valor)
+        {
+            contas[this.codigo].depositar(valor);
+        }
+
+        public string getContas()
+        {
+            string str = "";
+            foreach (Conta ct in contas)
+            {
+                str += ct.numero;
+                str += "\n";
+            }
+            return str;
+        }
+    }
+}
--- banco/banco/Program.cs
+++ banco/banco/Program.cs
@@ -10,64 +10,80 @@ namespace banco
     {
         static void Main(string[] args)
         {
-            dynamic ct = new ContaCorrente(0,0);
             double valor;
+            Caixa cx = new Caixa();
+            Menu:
+            for (;;)
+            {
+                Console.WriteLine("Opcoes");
+                Console.WriteLine("1 - Criar conta corrente");
+                Console.WriteLine("2 - Criar conta poupanca");
+                Console.WriteLine("3 - Ver contas");
+                Console.WriteLine("0 - Encerrar operacao");
+                int opcao = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Opcoes");
-            Console.WriteLine("1 - Criar conta corrente");
-            Console.WriteLine("2 - Criar conta poupanca");
-            int opcao = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Numero:");
-            int numero = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Deposito inicial:");
-            double saldo = Convert.ToDouble(Console.ReadLine());
-           
-           if (opcao == 1)
-           {
-                ct = new ContaCorrente(numero, saldo);
-           }
-           else if (opcao == 2)
-           {
-                ct = new ContaPoupanca(numero, saldo);
-           }
-           else
-           {
-                Console.WriteLine("Opcao invalida!");
-                opcao = -1;
-           }
-
-           while (opcao != 0)
-           {
-                Console.WriteLine("1 - Depositar");
-                Console.WriteLine("2 - Sacar");
-                Console.WriteLine("3 - Saldo");
-                Console.WriteLine("0 - Sair");
-                opcao = Convert.ToInt32(Console.ReadLine()); 
-                switch (opcao)
+                if (opcao == 1 || opcao == 2)
                 {
-                    case 1:
-                        Console.WriteLine("Valor do deposito:");
-                        valor = Convert.ToDouble(Console.ReadLine());
-                        ct.depositar(valor);
-                        break;
-                    case 2:
-                        Console.WriteLine("Valor do saque:");
-                        valor = Convert.ToDouble(Console.ReadLine());
-                        ct.sacar(valor);
-                        break;
-                    case 3:
-                        Console.WriteLine("Saldo: R$" + ct.saldo);
-                        break;
-                    case 0:
-                        Console.WriteLine("Tchau!");
-                        break;
-                    default:
-                        Console.WriteLine("Opcao invalida!");
-                        break;
+                    Console.WriteLine("Deposito inicial:");
+                    double saldo = Convert.ToDouble(Console.ReadLine());
+                    if (opcao == 1)
+                    {
+                        Console.WriteLine("Numero da conta: {0}", cx.criarContaCorrente(saldo));
+                        continue;
+                    }
+                    else if (opcao == 2)
+                    {
+                        Console.WriteLine("Numero da conta: {0}", cx.criarContaPoupanca(saldo));
+                        continue;
+                    }
                 }
-           }
+                else if (opcao == 3) 
+                {
+                    Console.WriteLine("Selecione a conta desejada: " + "\n" + cx.getContas());
+                    int conta = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(cx.setConta(conta));
+                }
+                else if (opcao == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opcao invalida!");
+                    break;
+                }
 
+                MenuConta:
+                for (; ; )
+                {
+                    Console.WriteLine("1 - Depositar");
+                    Console.WriteLine("2 - Sacar");
+                    Console.WriteLine("3 - Saldo");
+                    Console.WriteLine("0 - Sair");
+                    opcao = Convert.ToInt32(Console.ReadLine());
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.WriteLine("Valor do deposito:");
+                            valor = Convert.ToDouble(Console.ReadLine());
+                            cx.depositar(valor);
+                            goto MenuConta;
+                        case 2:
+                            Console.WriteLine("Valor do saque:");
+                            valor = Convert.ToDouble(Console.ReadLine());
+                            cx.sacar(valor);
+                            goto MenuConta;
+                        case 3:
+                            Console.WriteLine("Saldo: R${0}", cx.getSaldo());
+                            goto MenuConta;
+                        case 0:
+                            goto Menu;
+                        default:
+                            Console.WriteLine("Opcao invalida!");
+                            goto MenuConta;
+                    }
+                }
+            }
         }
     }
 }
